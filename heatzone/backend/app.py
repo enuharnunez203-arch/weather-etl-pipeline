987bf40360a0c5ffeb2adabebe2b76e51934c77d
enuharnunez203-arch <enuharnunez203@gmail.com>
2026-06-10 20:36:46 -0600

    Flask fixed

--- heatzone/backend/app.py
+++ heatzone/backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 import requests
 import os
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__, static_folder='..', static_url_path='')
 
 @app.route('/')
 def index():
